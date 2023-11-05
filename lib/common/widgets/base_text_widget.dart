5b2a9fe6b27fb8a97aa79cb2fe192c171de50787
lohcy <loh.cheeyung96@gmail.com>
2023-11-05 21:31:44 +0800

    fix path issue

--- lib/common/widgets/base_text_widget.dart
+++ lib/common/widgets/base_text_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:ulearning1/common/values/colors.dart';
+import 'package:food_delivery/common/values/colors.dart';
 
 Widget reusableText(String text,
     {Color color = AppColors.primaryText,

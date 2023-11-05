8617b2453a94731081ba17989c510c43529d1256
lohcy <loh.cheeyung96@gmail.com>
2023-11-05 21:27:57 +0800

    import common text widget

---
+++ lib/common/widgets/base_text_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning1/common/values/colors.dart';
+
+Widget reusableText(String text,
+    {Color color = AppColors.primaryText,
+    double fontSize = 16,
+    FontWeight fontWeight = FontWeight.bold}) {
+  return Container(
+    child: Text(
+      text,
+      style: TextStyle(
+          color: color, fontWeight: fontWeight, fontSize: fontSize.sp),
+    ),
+  );
+}

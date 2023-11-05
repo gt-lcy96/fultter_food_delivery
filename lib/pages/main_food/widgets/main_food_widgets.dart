91465344561ad7f99c4cc6b6cfe6954403272d8f
lohcy <loh.cheeyung96@gmail.com>
2023-11-05 22:14:19 +0800

    finish up floating title

--- lib/pages/main_food/widgets/main_food_widgets.dart
+++ lib/pages/main_food/widgets/main_food_widgets.dart
@@ -4,38 +4,48 @@ import 'package:food_delivery/common/values/colors.dart';
 import 'package:food_delivery/common/widgets/base_text_widget.dart';
 
 Widget mainTitleBar() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Container(
-        margin: EdgeInsets.only(left: 15.w, top:10.h),
-        child: Column(
-          children: [
-            reusableText(
-              "Bangladesh",
-              fontSize: 18.sp,
-              color: AppColors.primaryElement,
-            ),
-            Row(
+  return Container(
+    width: 350.w,
+    height: 100.h,
+    child: Stack(
+      children: [
+        Positioned(
+          left: 20,
+          top: 20,
+          child: Container(
+            child: Column(
               children: [
-                reusableText("Narshingdi", fontSize: 12.sp, fontWeight:FontWeight.normal),
-                Icon(Icons.arrow_drop_down)
+                reusableText(
+                  "Bangladesh",
+                  fontSize: 18.sp,
+                  color: AppColors.primaryElement,
+                ),
+                Row(
+                  children: [
+                    reusableText("Narshingdi", fontSize: 12.sp, fontWeight:FontWeight.normal),
+                    Icon(Icons.arrow_drop_down)
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
-      ),
-      Container(
-        margin: EdgeInsets.only(right: 15.w),
-        width: 50.w,
-        height: 45.h,
-        
-        decoration: BoxDecoration(
-          color: AppColors.primaryElement,
-          borderRadius: BorderRadius.circular(16.w),
-        ),
-        child: Icon(Icons.search, color: AppColors.primaryBackground),
-      )
-    ],
+        Positioned(
+          right: 5.w,
+          top: 10.h,
+          child: Container(
+            margin: EdgeInsets.only(right: 15.w),
+            width: 50.w,
+            height: 40.h,
+            
+            decoration: BoxDecoration(
+              color: AppColors.primaryElement,
+              borderRadius: BorderRadius.circular(16.w),
+            ),
+            child: Icon(Icons.search, color: AppColors.primaryBackground),
+          ),
+        )
+      ],
+    ),
   );
 }

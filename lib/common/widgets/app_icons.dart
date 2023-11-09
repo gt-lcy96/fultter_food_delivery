b9b7a086edd3c542ffd0cfb8a7c61bd7bfbb24fc
lohcy <loh.cheeyung96@gmail.com>
2023-11-09 16:02:35 +0800

    create bottomNavigationbar for recommended_food_detail

--- lib/common/widgets/app_icons.dart
+++ lib/common/widgets/app_icons.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppIcon extends StatelessWidget {
-  const AppIcon(
-      {super.key,
-      required this.icon,
-      this.size = 40,
-      this.iconColor = const Color(0xFF756d54),
-      this.backgroundColor = const Color(0xFFfcf4e4)});
+  const AppIcon({
+    super.key,
+    required this.icon,
+    this.size = 40,
+    this.iconColor = const Color(0xFF756d54),
+    this.backgroundColor = const Color(0xFFfcf4e4),
+    this.iconSize = 16,
+  });
 
   final IconData icon;
   final double size;
   final Color iconColor;
   final Color backgroundColor;
+  final double iconSize;
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +23,10 @@ class AppIcon extends StatelessWidget {
       width: size.w,
       height: size.h,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(size.w/2),
+        borderRadius: BorderRadius.circular(size.w / 2),
         color: backgroundColor,
       ),
-      child: Icon(icon,color: iconColor, size:16.w),
+      child: Icon(icon, color: iconColor, size: iconSize.w),
     );
   }
 }
--- lib/pages/food_detail/recommended_food_detail.dart
+++ lib/pages/food_detail/recommended_food_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_delivery/common/values/colors.dart';
 import 'package:food_delivery/common/widgets/app_icons.dart';
 import 'package:food_delivery/common/widgets/base_text_widget.dart';
+import 'package:food_delivery/pages/food_detail/widgets/PopularFoodDetail_widget.dart';
 import 'package:food_delivery/pages/food_detail/widgets/expandable_text_widget.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
@@ -50,13 +51,67 @@ class RecommendedFoodDetail extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                margin: EdgeInsets.only(left: 20.w, right: 20.w),
-                child: ExpandableTextWidget(lineHeight: 1.5, text: "In the ever-expanding realm of culinary delights, few dishes can claim to have achieved the iconic status that pizza enjoys. Picture this: a symphony of flavors, a harmony of textures, all orchestrated on a canvas of perfectly baked dough. Welcome to Pizza Paradise, where each slice is a passport to a world of gastronomic ecstasy.At the heart of our menu lies a commitment to quality ingredients. Our dough is a labor of love, crafted with precision and patience to achieve the ideal balance of chewiness and crispiness. The foundation of any good pizza, our dough serves as the canvas upon which we paint a masterpiece of flavors.")
-              )
+                  margin: EdgeInsets.only(left: 20.w, right: 20.w),
+                  child: ExpandableTextWidget(
+                      lineHeight: 1.5,
+                      text:
+                          "In the ever-expanding realm of culinary delights, few dishes can claim to have achieved the iconic status that pizza enjoys. Picture this: a symphony of flavors, a harmony of textures, all orchestrated on a canvas of perfectly baked dough. Welcome to Pizza Paradise, where each slice is a passport to a world of gastronomic ecstasy.At the heart of our menu lies a commitment to quality ingredients. Our dough is a labor of love, crafted with precision and patience to achieve the ideal balance of chewiness and crispiness. The foundation of any good pizza, our dough serves as the canvas upon which we paint a masterpiece of flavors."))
             ],
           ),
         ),
       ]),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 50.w),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppIcon(
+                      iconSize: 24,
+                      iconColor: Colors.white,
+                      backgroundColor: AppColors.primaryElement,
+                      icon: Icons.remove),
+                  bigText("\$12.88" + "  X  " + "0",
+                      color: Colors.black, fontSize: 22.sp),
+                  AppIcon(
+                      iconSize: 24,
+                      iconColor: Colors.white,
+                      backgroundColor: AppColors.primaryElement,
+                      icon: Icons.add),
+                ]),
+          ),
+          Container(
+            height: 100.h,
+            padding: EdgeInsets.only(
+                top: 15.h, bottom: 15.h, left: 20.w, right: 20.w),
+            decoration: BoxDecoration(
+                color: AppColors.primarySecondaryElementText.withOpacity(0.1),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.w * 2),
+                  topRight: Radius.circular(20.w * 2),
+                )),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(15.w),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.primaryElement,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.w),
+                  ),
+                ),
+                addToCartWithPrice_button(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

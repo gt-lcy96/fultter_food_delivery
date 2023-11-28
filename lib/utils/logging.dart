ff1a5a3bac72fa6fdedda64dde6378ca4d9796b7
lohcy <loh.cheeyung96@gmail.com>
2023-11-28 18:07:59 +0800

    add logging prettyPrintJsonEncodedStringList

--- lib/utils/logging.dart
+++ lib/utils/logging.dart
@@ -5,3 +5,9 @@ void prettyPrintJsonEncodedString(String jsonString) {
     var prettyString = JsonEncoder.withIndent('  ').convert(jsonObject);
     print(prettyString);
 }
+
+void prettyPrintJsonEncodedStringList(List<String> jsonStringList) {
+  for(var i=0; i< jsonStringList!.length; i++) {      
+      prettyPrintJsonEncodedString(jsonStringList[i]);
+    }
+}

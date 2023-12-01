71e242cd4fbf33927f3a1e15fab428e49c331275
lohcy <loh.cheeyung96@gmail.com>
2023-12-01 16:39:52 +0800

    add RefreshIndicator

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/controllers/recommended_product_controller.dart';
 import 'package:food_delivery/pages/home/widgets/main_food_widgets.dart';
 import 'package:food_delivery/pages/home/FoodPageSlider.dart';
+import 'package:get/get.dart';
 
 class MainFoodPage extends StatelessWidget {
   const MainFoodPage({super.key});
 
+  Future<void> _loadResources() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            mainTitleBar(),
-            const FoodPageSlider(),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: recommendedTitleTexts(),
-            ),
-            Expanded(child: SingleChildScrollView(child: recommendedSuggestList())),
-          ],
+      body: RefreshIndicator(
+        onRefresh: _loadResources,
+        child: SafeArea(
+          child: Column(
+            children: [
+              mainTitleBar(),
+              const FoodPageSlider(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: recommendedTitleTexts(),
+              ),
+              Expanded(child: SingleChildScrollView(child: recommendedSuggestList())),
+            ],
+          ),
         ),
       ),
     );

45672af2ae94e94e76b548249af66f250157fb71
lohcy <loh.cheeyung96@gmail.com>
2023-12-03 21:15:47 +0800

    create CustomLoader for loading

---
+++ lib/common/widgets/customLoader.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/common/values/colors.dart';
+
+class CustomLoader extends StatelessWidget {
+  const CustomLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 100.h,
+        width: 100.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: AppColors.primaryElement
+        ),
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(color: Colors.white),
+      ),
+    );
+  }
+}
--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -5,6 +5,7 @@ import 'package:food_delivery/common/values/colors.dart';
 import 'package:food_delivery/common/values/dimensions.dart';
 import 'package:food_delivery/common/widgets/app_text_field.dart';
 import 'package:food_delivery/common/widgets/base_text_widget.dart';
+import 'package:food_delivery/common/widgets/customLoader.dart';
 import 'package:food_delivery/common/widgets/showCustomSnackBar.dart';
 import 'package:food_delivery/controllers/auth_controller.dart';
 import 'package:food_delivery/models/signup_body_model.dart';
@@ -26,8 +27,7 @@ class SignUpPage extends StatelessWidget {
       "facebook.png",
     ];
 
-    void _registration() {
-      var authController = Get.find<AuthController>();
+    void _registration(AuthController authController) {
       String username = usernameController.text.trim();
       String phone = phoneController.text.trim();
       String email = emailController.text.trim();
@@ -72,100 +72,104 @@ class SignUpPage extends StatelessWidget {
         backgroundColor: Colors.white,
         body: GetBuilder<AuthController>(
           builder: (_authController) {
-            return !_authController.isLoading ? SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Column(
-                children: [
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.25,
-                    child: Center(
-                      child: CircleAvatar(
-                        radius: 80.w,
-                        backgroundImage: const AssetImage(
-                            "assets/images/food_delivery_logo.png"),
-                        backgroundColor: Colors.white,
-                      ),
-                    ),
-                  ),
-                  AppTextField(
-                      textController: emailController,
-                      hintText: "Email",
-                      icon: Icons.email),
-                  SizedBox(height: 20.h),
-                  AppTextField(
-                      textController: passwordController,
-                      hintText: "Password",
-                      icon: Icons.password_sharp),
-                  SizedBox(height: 20.h),
-                  AppTextField(
-                      textController: usernameController,
-                      hintText: "Username",
-                      icon: Icons.person),
-                  SizedBox(height: 20.h),
-                  AppTextField(
-                      textController: phoneController,
-                      hintText: "Phone",
-                      icon: Icons.phone),
-                  SizedBox(height: 20.h),
-                  //sign up button
-                  GestureDetector(
-                    onTap: () {
-                      _registration();
-                    },
-                    child: Container(
-                      width: Dimensions.screenWidth / 2,
-                      height: Dimensions.screenHeight / 13,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30.w),
-                          color: AppColors.primaryElement),
-                      child: Center(
-                        child: bigText("Sign up",
-                            fontSize: 30, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10.h),
-                  //tag line
-                  RichText(
-                    text: TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () => Get.back(),
-                      text: "Have an account already?",
-                      style: TextStyle(
-                        color: Colors.grey[500],
-                        fontSize: 20.sp,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: Dimensions.screenHeight * 0.01),
-                  //sign up options
-                  RichText(
-                    text: TextSpan(
-                      text: "Sign up using one of the following methods",
-                      style: TextStyle(
-                        color: Colors.grey[500],
-                        fontSize: 16.sp,
-                      ),
-                    ),
-                  ),
+            return !_authController.isLoading
+                ? SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01),
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.25,
+                          child: Center(
+                            child: CircleAvatar(
+                              radius: 80.w,
+                              backgroundImage: const AssetImage(
+                                  "assets/images/food_delivery_logo.png"),
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                        AppTextField(
+                            textController: emailController,
+                            hintText: "Email",
+                            icon: Icons.email),
+                        SizedBox(height: 20.h),
+                        AppTextField(
+                            textController: passwordController,
+                            hintText: "Password",
+                            icon: Icons.password_sharp),
+                        SizedBox(height: 20.h),
+                        AppTextField(
+                            textController: usernameController,
+                            hintText: "Username",
+                            icon: Icons.person),
+                        SizedBox(height: 20.h),
+                        AppTextField(
+                            textController: phoneController,
+                            hintText: "Phone",
+                            icon: Icons.phone),
+                        SizedBox(height: 20.h),
+                        //sign up button
+                        GestureDetector(
+                          onTap: () {
+                            _registration(_authController);
+                          },
+                          child: Container(
+                            width: Dimensions.screenWidth / 2,
+                            height: Dimensions.screenHeight / 13,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.w),
+                                color: AppColors.primaryElement),
+                            child: Center(
+                              child: bigText("Sign up",
+                                  fontSize: 30, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10.h),
+                        //tag line
+                        RichText(
+                          text: TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () => Get.back(),
+                            text: "Have an account already?",
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                              fontSize: 20.sp,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: Dimensions.screenHeight * 0.01),
+                        //sign up options
+                        RichText(
+                          text: TextSpan(
+                            text: "Sign up using one of the following methods",
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                              fontSize: 16.sp,
+                            ),
+                          ),
+                        ),
 
-                  Wrap(
-                    children: List.generate(
-                        3,
-                        (index) => Padding(
-                              padding: EdgeInsets.only(left: 8.w, right: 8.w),
-                              child: CircleAvatar(
-                                radius: 25.w,
-                                backgroundImage: AssetImage(
-                                    "assets/images/" + signUpImages[index]),
-                              ),
-                            )),
+                        Wrap(
+                          children: List.generate(
+                              3,
+                              (index) => Padding(
+                                    padding:
+                                        EdgeInsets.only(left: 8.w, right: 8.w),
+                                    child: CircleAvatar(
+                                      radius: 25.w,
+                                      backgroundImage: AssetImage(
+                                          "assets/images/" +
+                                              signUpImages[index]),
+                                    ),
+                                  )),
+                        )
+                      ],
+                    ),
                   )
-                ],
-              ),
-            )
-          : const CircularProgressIndicator();
+                : const CustomLoader();
           },
         ));
   }

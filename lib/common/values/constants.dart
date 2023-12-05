ccce027e30642bc1a8054df4db9d87c8e40ff2c4
lohcy <loh.cheeyung96@gmail.com>
2023-12-05 14:11:37 +0800

    set to localHost BASE_URL

--- lib/common/values/constants.dart
+++ lib/common/values/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
-  // static const String BASE_URL = "http://127.0.0.1:8000";
-  static const String BASE_URL = "http://192.168.68.103:8000";
+  static const String BASE_URL = "http://127.0.0.1:8000";
+  // static const String BASE_URL = "http://192.168.68.103:8000";
   static const POPULAR_TYPE_ID = 2;
   static const RECOMMENDED_TYPE_ID = 3;
   static const String POPULAR_PRODUCT_URI = "/api/product-containers/type/${POPULAR_TYPE_ID}";

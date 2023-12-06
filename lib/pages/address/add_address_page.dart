caf84fc3bafdf379fb1828e98c3a54f6efaa1c71
lohcy <loh.cheeyung96@gmail.com>
2023-12-06 16:24:38 +0800

    create TextField for contact name and number

--- lib/pages/address/add_address_page.dart
+++ lib/pages/address/add_address_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_delivery/common/values/colors.dart';
+import 'package:food_delivery/common/widgets/app_text_field.dart';
+import 'package:food_delivery/common/widgets/base_text_widget.dart';
 import 'package:food_delivery/controllers/auth_controller.dart';
 import 'package:food_delivery/controllers/location_controller.dart';
 import 'package:food_delivery/controllers/user_controller.dart';
@@ -51,39 +53,90 @@ class _AddAddressPageState extends State<AddAddressPage> {
         title: Text("Address page"),
         backgroundColor: AppColors.primaryElement,
       ),
-      body: GetBuilder<LocationController>(
-        builder: (locationController) {
-          return Column(children: [
-            Container(
-              height: 140.h,
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.only(left: 5.w, right: 5.w, top: 5.h),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(
-                    width: 2.w,
-                    color: Theme.of(context).primaryColor,
-                  )),
-              child: Stack(children: [
-                GoogleMap(
-                    initialCameraPosition:
-                        CameraPosition(target: _initialPosition, zoom: 17),
-                    zoomControlsEnabled: false,
-                    compassEnabled: false,
-                    indoorViewEnabled: true,
-                    mapToolbarEnabled: false,
-                    onCameraIdle: () {
-                      locationController.updatePosition(_cameraPosition,  true);
-                    },
-                    onCameraMove: ((position) => _cameraPosition = position),
-                    onMapCreated: (GoogleMapController controller) {
-                      locationController.setMapController(controller);
-                    }),
-              ]),
-            ),
-          ]);
-        },
-      ),
+      body: GetBuilder<UserController>(builder: (userController) {
+        if(userController.userModel != null && _contactPersonName.text.isEmpty) {
+          _contactPersonName.text = '${userController.userModel?.username}';
+          _contactPersonNumber.text = '${userController.userModel?.phone}';
+          if(Get.find<LocationController>().addressList.isNotEmpty) {
+            _addressController.text = Get.find<LocationController>().getUserAddress().address;
+          }
+        }
+        return GetBuilder<LocationController>(
+          builder: (locationController) {
+            _addressController.text =
+                '${locationController.placemark.name ?? ""}'
+                '${locationController.placemark.locality ?? ""}'
+                '${locationController.placemark.postalCode ?? ""}'
+                '${locationController.placemark.country ?? ""}';
+            print("address in my view is " + _addressController.text);
+
+            return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 140.h,
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.only(left: 5.w, right: 5.w, top: 5.h),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          width: 2.w,
+                          color: AppColors.primaryElement,
+                        )),
+                    child: Stack(children: [
+                      GoogleMap(
+                          initialCameraPosition: CameraPosition(
+                              target: _initialPosition, zoom: 17),
+                          zoomControlsEnabled: false,
+                          compassEnabled: false,
+                          indoorViewEnabled: true,
+                          mapToolbarEnabled: false,
+                          onCameraIdle: () {
+                            locationController.updatePosition(
+                                _cameraPosition, true);
+                          },
+                          onCameraMove: ((position) =>
+                              _cameraPosition = position),
+                          onMapCreated: (GoogleMapController controller) {
+                            locationController.setMapController(controller);
+                          }),
+                    ]),
+                  ),
+                  SizedBox(height: 20.h),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: bigText("Delivery address"),
+                  ),
+                  SizedBox(height: 10.h),
+                  AppTextField(
+                      textController: _addressController,
+                      hintText: "Your address",
+                      icon: Icons.map),
+                  SizedBox(height: 20.h),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: bigText("Contact name"),
+                  ),
+                  SizedBox(height: 10.h),
+                  AppTextField(
+                      textController: _contactPersonName,
+                      hintText: "Your name",
+                      icon: Icons.person),
+                  SizedBox(height: 20.h),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: bigText("Your number"),
+                  ),
+                  SizedBox(height: 10.h),
+                  AppTextField(
+                      textController: _contactPersonNumber,
+                      hintText: "Your Phone",
+                      icon: Icons.phone),
+
+                ]);
+          },
+        );
+      }),
     );
   }
 }
